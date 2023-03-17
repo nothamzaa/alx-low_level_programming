fee36b0dba6a37955ece6723d21e325b7e61766b
hamza <hamzabousseta1@gmail.com>
2023-03-17 11:36:55 -0700

    resolve code er for task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Description: checking if positive or negative
+ * Return: always 0
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
@@ -10,13 +15,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else if (n > 0)
+	if (n > 0)
 	{
 		printf("%d is posetive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
 	}
 	else
 		printf("%d is zero\n", n);
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-C variables task project 
+ projct 0x01-variables_if_else_while tasks 
